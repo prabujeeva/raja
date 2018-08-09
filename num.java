7434347524e58a8719998cd97bc553d6644d24c4
prabujeeva <42169857+prabujeeva@users.noreply.github.com>
2018-08-09 12:45:29 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,18 @@
+class num
+{
+public static void main(String args[])
+{
+  int a=2;
+  if(a>0)
+  {
+    system.out.println("number is positive");
+   }
+  else if(a<0)
+  {
+    system.out.println("number is negative);
+  }
+  else(a=0)
+    {
+     system.out.println("given number is zero");
+     }
+     }}
